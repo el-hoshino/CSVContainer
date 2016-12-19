e009b079fff554fbad2b5e7f96c67db71a7b4158
史 翔新 <shi_xiangxin@mages.co.jp>
2016-12-19 17:12:06 +0900

    Initial settings

---
+++ CSVContainer/CSVContainer/CSVContainer.h
@@ -0,0 +1,19 @@
+//
+//  CSVContainer.h
+//  CSVContainer
+//
+//  Created by 史　翔新 on 2016/12/19.
+//  Copyright © 2016年 crazism. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CSVContainer.
+FOUNDATION_EXPORT double CSVContainerVersionNumber;
+
+//! Project version string for CSVContainer.
+FOUNDATION_EXPORT const unsigned char CSVContainerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSVContainer/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSVContainer
+This is a CSV data structure definition that can be easily loaded from a CSV file, modifying data and outputting to a CSV file.
